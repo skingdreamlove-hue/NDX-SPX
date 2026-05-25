d1ed26deb7b9f960195dd07a5949ccea3e2921bc
skingdreamlove-hue <277157647+skingdreamlove-hue@users.noreply.github.com>
2026-05-25 20:06:31 +0000

    auto: 更新市场数据

--- docs/market_data.js
+++ docs/market_data.js
@@ -28,5 +28,5 @@ var MARKET_DATA = {
   "on_rrp_deviation": null,
   "sp500_breadth_200ma": null,
   "ndx_breadth_200ma": null,
-  "last_update": "2026-05-25 16:36:49"
+  "last_update": "2026-05-25 20:06:31"
 };
